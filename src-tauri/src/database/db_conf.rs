68dc380858bc726c292c4707464f5ff4562cd23f
Alaric-Jeff <jefsohandsome1@gmail.com>
2025-08-08 10:26:01 +0800

    added chrono, new mods

--- src-tauri/src/database/db_conf.rs
+++ src-tauri/src/database/db_conf.rs
@@ -3,9 +3,32 @@ use once_cell::sync::Lazy;
 use std::sync::Mutex;
 
 pub static DB: Lazy<Mutex<Connection>> = Lazy::new(|| {
-    Mutex::new(Connection::open("chef_fusion.db").expect("Failed to open DB"));
+    Mutex::new(Connection::open("chef_fusion.db").expect("Failed to open DB"))
 });
 
-pub fn setup_database(){
-    let conn = DB.lock.unwrap();
+pub fn setup_database() {
+    let conn = DB.lock().unwrap();
+
+    conn.execute(
+        "CREATE TABLE IF NOT EXISTS menu_table (
+            menu_id        INTEGER PRIMARY KEY AUTOINCREMENT,
+            menu_name      TEXT NOT NULL,
+            menu_price     REAL NOT NULL,
+            category       TEXT,
+            inv_id         TEXT, 
+            total_serving  INTEGER NOT NULL,
+            date           TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+        )",
+        [],
+    ).expect("Error in creating menu_table");
+
+    conn.execute(
+        "CREATE TABLE IF NOT EXISTS order_table (
+            order_id   INTEGER PRIMARY KEY AUTOINCREMENT,
+            menu_id    INTEGER NOT NULL,
+            price      REAL NOT NULL DEFAULT 0,
+            date       TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+        )",
+        [],
+    ).expect("Error in creating order_table");
 }
--- src-tauri/src/database/mod.rs
+++ src-tauri/src/database/mod.rs
@@ -1,3 +1,4 @@
 pub mod db_conf;
 
 pub use db_conf::DB;
+pub use db_conf::setup_database;
